98b0e785e4d81f1616baf2b2d76ed4020cd9ba1c
AlexeyVibe <alexvibe.av@gmail.com>
2023-05-24 22:23:16 +0300

    swagger

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 
 async function bootstrap() {
   const PORT = process.env.PORT || 5000;
@@ -9,6 +10,15 @@ async function bootstrap() {
       origin: true,
     },
   });
+
+  const config = new DocumentBuilder()
+      .setTitle('ivi clone')
+      .setDescription('Документащия REST API')
+      .setVersion('1.0.0')
+      .build()
+  const document = SwaggerModule.createDocument(app,config);
+  SwaggerModule.setup('/api/docs',app,document)
+
   await app.listen(PORT, () => console.log('Server started on port =' + PORT))
 }
 
--- apps/auth-users/src/main.ts
+++ apps/auth-users/src/main.ts
@@ -15,6 +15,7 @@ async function start() {
       },
     },
   });
+
   app.listen();
 }
 
